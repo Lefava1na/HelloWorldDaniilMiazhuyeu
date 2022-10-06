f950ba43e08eaf9926a5e131a5f23673a62ed865
DaniilMiazhuyeu <dmiazhu1@stu.vistula.edu.pl>
2022-10-06 16:00:24 +0200

    Commit 2

--- src/pl/vistula/helloworldmiazhuyeu/HelloWorldMiazhuyeu.java
+++ src/pl/vistula/helloworldmiazhuyeu/HelloWorldMiazhuyeu.java
@@ -4,5 +4,8 @@ public class HelloWorldMiazhuyeu {
     public static void main(String[] args) {
 
         System.out.println ("Hello Vistula Miazhuyeu");
+
+        System.out.println ("Hello Vistula Miazhuyeu");
+
     }
 }
